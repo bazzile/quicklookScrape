e413a93b40b7f232505da87a45101e413b295e31
lobanov.office <bazzzzile@gmail.com>
2016-06-10 19:52:10 +0300

    убран лишний код

--- progress_bar.py
+++ progress_bar.py
@@ -18,17 +18,7 @@ from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, \
 img_url = 'https://browse.digitalglobe.com/imagefinder/showBrowseImage?catalogId=104001000C880D00&imageHeight=natres&imageWidth=natres'
 out_dir = r"E:\!"
 
-
-# def example4():
-#     widgets = ['Test: ', Percentage(), ' ',
-#                Bar(marker='=', left='[', right=']'),
-#                ' ', ETA(), ' ', FileTransferSpeed()]
-#     pbar = ProgressBar(widgets=widgets, max_value=500)
-#     pbar.start()
-#     for i in range(100, 500 + 1, 50):
-#         time.sleep(0.001)
-#         pbar.update(i)
-#     pbar.finish()
+# TODO убрать или видоизменить функцию, добавить filename после 'Загружаем: ' Объём файла тоже можно отображать
 
 
 def downloader():
@@ -51,14 +41,3 @@ def downloader():
             i.save(os.path.join(out_dir, 'image.jpg'), quality=85)
 
 downloader()
-
-# r = requests.get(img_url, stream=True)
-# if r.status_code == 200:
-#     length = int(r.headers['Content-Length'])
-#     with TemporaryFile() as tempf:
-#         for chunk in r.iter_content(1024):
-#             length -= len(chunk)
-#             print(length)
-#             tempf.write(chunk)
-#         i = Image.open(tempf)
-#         i.save(os.path.join(out_dir, 'image.jpg'), quality=85)
